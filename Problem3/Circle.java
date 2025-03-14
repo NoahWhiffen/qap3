8cd97b058bc44ab2d2d3c5478eacb666a9cf062a
NoahWhiffen <Noah.Whiffen@keyin.com>
2025-03-14 12:25:43 -0230

    Circle class and methods completed

--- Problem3/Circle.java
+++ Problem3/Circle.java
@@ -1,5 +1,18 @@
 package Problem3;
 
 class Circle extends Ellipse {
-    
+    public Circle(double radius) {
+        // Since a circle is an ellipse with a = b, we call the Ellipse constructor with radius for both axes
+        super(radius, radius);
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * a * a;
+    }
+
+    @Override
+    public double perimeter() {
+        return 2 * Math.PI * a;
+    }
 }
